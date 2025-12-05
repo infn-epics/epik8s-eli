0d1707a2c3906b9e6eb5f6c50a170b28ed15e26b
amichelo <andrea.michelotti@lnf.infn.it>
2025-12-05 10:30:07 +0200

    modified orbit conf, added bpm to alarm

--- config/iocs/orbit/start.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-script_dir=$(dirname "$0")
-cd $script_dir
-echo "Starting Orbit addr: $EPICS_CA_ADDR_LIST"
-python ./scripts/py-ioc-collector.py -c sparc_orbit.json -p __CONFIG_DIR__/pvlist.txt
-echo "Exited..."
