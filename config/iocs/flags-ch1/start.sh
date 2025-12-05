37770d2e1fb30f1679fbd5113bb51e2a7ca93ee6
amichelo <andrea.michelotti@lnf.infn.it>
2025-12-05 11:12:53 +0200

    removed unused directories

--- config/iocs/flags-ch1/start.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-script_dir=$(dirname "$0")
-cd $script_dir
-pip install -r scripts/requirements.txt
-echo "Starting __IOC_NAME__"
-python ./scripts/pymotflag.py sparc-config.yaml -p __CONFIG_DIR__/pvlist.txt
-echo "Exited..."
--- config/iocs/libera-llrf/start.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-mount -o remount ,rw /
-ntpdate ntp1.lnf.infn.it
-./st.cmd
